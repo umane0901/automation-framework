d925802abb31dbafe3bdbfdf6147d881259117e6
Umesh Mane <umane0901@gmail.com>
2026-01-24 16:21:01 +0530

    updated project

---
+++ src/test/java/com/orangehrm/tests/BaseClass.java
@@ -0,0 +1,5 @@
+package com.orangehrm.tests;
+
+public class BaseClass {
+
+}
---
+++ src/test/java/com/orangehrm/utils/DriverManager.java
@@ -0,0 +1,27 @@
+package com.orangehrm.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DriverManager {
+	
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal();
+	
+	public static void init(String browser) {
+		if(driver.get() == null) {
+			switch(browser.toLowerCase()) {
+				case "chrome":
+					driver.set(new ChromeDriver());
+				break;
+				case "edge":
+					driver.set(new EdgeDriver());
+			}
+		}
+	}
+	
+	 public static WebDriver getDriver() {
+		 return driver.get();
+	 }
+	
+}
